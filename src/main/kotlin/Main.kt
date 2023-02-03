db7446de65620d59ee147f4eb2534ded92f9627b
MiriamLongoyapus <longoyapusmiriam@gmail.com>
2023-02-03 08:29:54 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,12 @@
+fun main() {
+    var name = "Miriam"
+    println(name)
+    var age = 10
+    println(age)
+    var phoneNumber = "0724686395"
+    println(phoneNumber)
+    var weight = 40
+    println(weight)
+    var CitizenKenya = false
+    println(CitizenKenya)
+}
